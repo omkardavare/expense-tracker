e2dde320cacf597d9f27fdf77829729de84781cf
Omkar Davare <121477678+omkardavare@users.noreply.github.com>
2025-08-16 16:48:14 +0530

    Update app.py
    Supabase connection

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template, request, redirect
-import csv
+from supabase import create_client, Client
+from dotenv import load_dotenv
 import os
 
+# Load environment variables from .env
+load_dotenv()
+
 app = Flask(__name__)
-CSV_FILE = 'expenses.csv'
+
+# Supabase setup
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.route('/')
 def index():
-    data = []
-    if os.path.exists(CSV_FILE):
-        with open(CSV_FILE, newline='') as f:
-            reader = csv.DictReader(f)
-            data = list(reader)
+    # fetch all users from Supabase
+    response = supabase.table("expenses").select("*").execute()
+    data = response.data if response.data else []
     return render_template('index.html', data=data)
+
+
 
 @app.route('/add_expense', methods=['POST'])
 def add_expense():
@@ -23,22 +31,27 @@ def add_expense():
             'subcategory': request.form.get('subcategory'),
             'amount': request.form.get('amount'),
             'type': request.form.get('type'),
-            'payment_mode': request.form.get('payment_mode'),
+            'mode_of_payment': request.form.get('payment_mode'),
             'app_used': request.form.get('app_used'),
             'notes': request.form.get('notes')
         }
 
         # Ensure no required fields are missing
-        if not all([new_entry['date'], new_entry['category_name'], new_entry['subcategory'], new_entry['amount'], new_entry['type'], new_entry['payment_mode'], new_entry['app_used']]):
+        if not all([new_entry['date'], new_entry['category_name'], new_entry['subcategory'], new_entry['amount'], new_entry['type'], new_entry['mode_of_payment'], new_entry['app_used']]):
             return "Bad Request: Missing required fields", 400
 
-        file_exists = os.path.isfile(CSV_FILE)
-        with open(CSV_FILE, mode='a', newline='') as f:
-            writer = csv.DictWriter(f, fieldnames=new_entry.keys())
-            if not file_exists:
-                writer.writeheader()
-            writer.writerow(new_entry)
+        
+        data, count = supabase.table("expenses").insert(new_entry).execute()
 
-        return redirect('/')
+        if data:
+            print("Hello, Saved to Supabase ✅")
+            return redirect('/')
+        else:
+            return "Error saving to Supabase ❌"
+
+        # return redirect('/')
     except Exception as e:
         return f"Error: {e}", 500
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
